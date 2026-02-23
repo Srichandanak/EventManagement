36befd7ee97bf24b47cc718ac41c4e23aa8457bb
Srichandanak <145416849+Srichandanak@users.noreply.github.com>
2026-02-23 22:57:23 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- 1app.py
+++ 1app.py
@@ -760,6 +760,10 @@ def callback():
     return "Callback received", 200
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 
